0171f54ccb046cce446bc5169ee9ab0fa50bdb2f
Peter J Gleckler <u7510@login28.chn.perlmutter.nersc.gov>
2025-01-21 09:44:54 -0800

    adding RC

---
+++ RegisteredContent/DRCDP_RC.py
@@ -0,0 +1,27 @@
+import json
+
+source_id = {}
+source_id['source_id'] = {}
+
+key = 'LOCA2'
+source_id['source_id'][key] = {}
+source_id['source_id'][key]['source_name'] = 'LOCA'
+source_id['source_id'][key]['source_version_number'] = '2'
+source_id['source_id'][key]['institution_id'] = 'UCSD-SIO'
+source_id['source_id'][key]['region'] = ['north_america']
+
+key = 'LOCA2.1'
+source_id['source_id'][key] = {}
+source_id['source_id'][key]['source_name'] = 'LOCA'
+source_id['source_id'][key]['source_version_number'] = '2.1'
+source_id['source_id'][key]['institution_id'] = 'UCSD-SIO'
+source_id['source_id'][key]['region'] = ['north_america']
+
+key = 'STAR-ESDM-v1'
+source_id['source_id'][key] = {}
+source_id['source_id'][key]['source_name'] = 'STAR-ESDM'
+source_id['source_id'][key]['source_version_number'] = 'v1'
+source_id['source_id'][key]['institution_id'] = 'TTU'
+source_id['source_id'][key]['region'] = ['north_america']
+
+
