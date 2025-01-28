1311640a8ba6312302fb8b194d08ccf1707399e7
Peter Gleckler <gleckler1@gates.llnl.gov>
2025-01-28 14:14:50 -0800

    adding UCM

--- RegisteredContent/DRCDP_RC.py
+++ RegisteredContent/DRCDP_RC.py
@@ -18,6 +18,13 @@ source_id['source_id'][key]['source_version_number'] = '2.1'
 source_id['source_id'][key]['institution_id'] = 'UCSD-SIO'
 source_id['source_id'][key]['region'] = ['north_america']
 
+key = 'MACAV3'
+source_id['source_id'][key] = {}
+source_id['source_id'][key]['source_name'] = 'MACA'
+source_id['source_id'][key]['source_version_number'] = 'V3'
+source_id['source_id'][key]['institution_id'] = 'UCM-ACSL'
+source_id['source_id'][key]['region'] = ['north_america']
+
 key = 'STAR-ESDM-v1'
 source_id['source_id'][key] = {}
 source_id['source_id'][key]['source_name'] = 'STAR-ESDM'
@@ -28,5 +35,4 @@ source_id['source_id'][key]['region'] = ['north_america']
 json_object = json.dumps(source_id, indent=4)
 with open("DRCDP-RegisteredContent.json", "w") as outfile:
     outfile.write(json_object)
-
 
